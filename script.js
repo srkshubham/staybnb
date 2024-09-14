a4a64dc938806154f00cfb3710771483ee6900d6
srkshubham <132755400+srkshubham@users.noreply.github.com>
2024-09-14 12:40:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var navBar = document.getElementById("navBar");
+
+function togglebtn() {
+  navBar.classList.toggle("hidemenu");
+}
+
+// $(window).on("load", function(){
+//     $(".loader-wrapper").fadeOut("slow");
+// });
